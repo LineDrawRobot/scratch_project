1c87f68ada70f06f70df811492e08d92eab08faf
LineDrawRobot <wakabalab2022@gmail.com>
2025-07-15 17:18:54 +0900

    Add files via upload

--- startup/autodownloder.sh
+++ startup/autodownloder.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 cd ~/scratch/scratch-vm/src/extensions/scratch3_newblocks
-curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/index/index.js > index.js
-curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/pyserver/pyserver.py > pyserver.py
+if ! curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/index/index.js > index.tmp
+then
+echo "DL失敗しました"
+rm index.tmp
+else
+echo "DL成功しました"
+mv index.tmp index.js
+fi
+
+if ! curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/pyserver/pyserver.py > pyserver.tmp
+then
+echo "DL失敗しました"
+rm pyserver.tmp
+else
+echo "DL成功しました"
+mv pyserver.tmp pyserver.py
+fi
+
 python init.py
--- startup/init.py
+++ startup/init.py
@@ -3,11 +3,7 @@ from gpiozero import LED
 from time import sleep
 
 # Raspberry Pi の BCM 番号で使われるすべての GPIO（使用中のピンだけでも可）
-gpio_pins = [
-    2, 3, 4, 5, 6, 9, 10, 11,
-    12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27
-]
+gpio_pins = [5,6,13,19,26,16,20,21,25] 
 
 leds = [LED(pin) for pin in gpio_pins]
 
