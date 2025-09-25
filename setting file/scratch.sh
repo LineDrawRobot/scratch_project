dfbca3ec5ff502166a37277b37c48aee0e7b35e3
LineDrawRobot <wakabalab2022@gmail.com>
2025-09-25 13:40:51 +0900

    Update scratch.sh

--- setting file/scratch.sh
+++ setting file/scratch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 lxterminal -e "python ~/scratch/scratch-vm/src/extensions/scratch3_newblocks/pyserver.py; exec bash" &
-lxterminal -e "cd ~/scratch/scratch-gui; yarn start; exec bash" &
+lxterminal -e "cd ~/scratch/scratch-gui; npm start; exec bash" &
 
 if pgrep -f "chromium-browser" > /dev/null; then
     echo "すでにChromiumが開いています。起動しません。"
