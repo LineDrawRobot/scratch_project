cc990c2495f956a793c0adf3e20faa87ddaefbe0
LineDrawRobot <wakabalab2022@gmail.com>
2025-09-26 18:15:42 +0900

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -18,24 +18,25 @@ cd ../scratch-gui
 npm install
 sudo npm link scratch-vm
 
+
 #スクラッチ拡張のインストール(scratch_vm関連）
 
-cd ../scratch-vm/src/extensions
+cd ~/scratch/scratch-vm/src/extensions
 mkdir scratch3_newblocks
 cd scratch3_newblocks
 curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/index/index.js > index.js
 curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/pyserver/pyserver.py > pyserver.py
-curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/blob/main/setting%20file/newblocks.png > newblocks.png
+curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/setting%20file/newblocks.png > newblocks.png
 
 #起動スクリプトのインストール
-curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/blob/main/setting%20file/scratch.sh > scratch.sh
-curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/blob/main/setting%20file/scratch2.sh > scratch2.sh
+curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/setting%20file/scratch.sh > scratch.sh
+curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/setting%20file/scratch2.sh > scratch2.sh
 
 sudo chmod +x scratch.sh
 sudo chmod +x scratch2.sh
 
-curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/blob/main/setting%20file/python.desktop > ~/Desktop/python.desktop
-curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/blob/main/setting%20file/scratch.desktop > ~/Desktop/scratch.desktop
+curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/setting%20file/python.desktop > ~/Desktop/python.desktop
+curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/setting%20file/scratch.desktop > ~/Desktop/scratch.desktop
 
 cd ~/scratch/scratch-vm/src/extension-support
 curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/index/extension-manager.js > extension-manager.js
@@ -45,8 +46,8 @@ cd ~/scratch/scratch-gui/src/lib/libraries/extensions
 curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/setting%20file/index.jsx > index.jsx
 mkdir newblocks
 cd newblocks
-curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/blob/main/setting%20file/newblocks.png > newblocks.png
-curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/blob/main/setting%20file/newblocks-small.png > newblocks-small.png
+curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/setting%20file/newblocks.png > newblocks.png
+curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/setting%20file/newblocks-small.png > newblocks-small.png
 
 #slackサーバーのインストール
 cd ~/scratch/scratch-vm/src/extensions/scratch3_newblocks
@@ -54,3 +55,13 @@ curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/
 curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/slack/slack_server.py > slack_server.py
 
 pip install slack-bolt --break-system-packages
+
+curl https://raw.githubusercontent.com/LineDrawRobot/scratch_project/refs/heads/main/slack/slack_server.service >slack_server.service
+sudo mv slack_server.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable slack_server.service
+sudo systemctl start slack_server.service
+
+#SPI有効化
+sudo raspi-config nonint do_spi 0
+
